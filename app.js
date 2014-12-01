6bd62f2fe4350eb73e98de16bf4764564b874409
ykyoon <xyzlast@gmail.com>
2014-12-01 11:12:41 +0900

    add tds data

--- app.js
+++ app.js
@@ -1,6 +1,45 @@
 var express = require('express'),
   config = require('./config/config');
 
+// var sql = require('mssql');
+// console.log('db connect start');
+// var conn = sql.connect({
+//   user: 'dazone',
+//   password: 'workcrew',
+//   server: '222.111.242.137',
+//   database: 'WorkCrewNET60c'
+// }, function(err) {
+//   if(err) {
+//     console.log('error');
+//     console.error(err);
+//   }
+// });
+
+var tds = require('tds');
+var conn = new tds.Connection({
+  host: '222.111.242.137',
+  port: 1433,
+  userName: 'dazone',
+  password: 'workcrew',
+  database: 'WorkCrewNET60c'
+});
+conn.connect(function(error) {
+  if (error != null) {
+    console.error('Received error', error);
+  } else {
+    console.log('Now connected, can start using');
+    var stmt = conn.createStatement('SELECT 1');
+    stmt.on('row', function(row) {
+      console.log('Received row: ', row.getValue(0));
+    });
+    stmt.execute();
+  }
+});
+
+
+
+
+
 var app = express();
 
 require('./config/express')(app, config);
